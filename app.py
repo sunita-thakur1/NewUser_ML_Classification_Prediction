aea811e3d1f5c82e17fb4455d71f52cc15340f79
sunita-thakur1 <67176792+sunita-thakur1@users.noreply.github.com>
2025-04-07 19:33:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -61,9 +61,10 @@ def train_model(df):
 df = load_data()
 st.write("### Preview of Data:")
 st.write(df.head())
+st.write("### Summary Statistics:")
 st.write(df.describe())
 st.write(df.groupby("Model_Used").agg({'sex': 'count',  'user_age': 'mean', 'user_cuisine': 'count', 'user_cuisine':'count', 'taste': 'count', 'Conversion_Rate (%)': 'mean', 'Likes': 'count' }))
-# Group by "Model_Used" and calculate the mean of "Conversion_Rate (%)"
+st.write("### Group by "Model_Used" and calculate the mean of "Conversion_Rate (%)")
 conversion_rate_summary = df.groupby("Model_Used")['Conversion_Rate (%)'].mean()
 
 # Find the model with the maximum conversion rate
