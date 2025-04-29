3abba50583075847eed680bf36e2098db2c9af90
sunita-thakur1 <67176792+sunita-thakur1@users.noreply.github.com>
2025-04-29 19:59:09 -0400

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt     
 import seaborn as sns
 import joblib
 from sklearn.model_selection import train_test_split
